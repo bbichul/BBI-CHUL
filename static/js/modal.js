d83a7ae217e6bc01a544def4d2dd7d9a50054d00
minjae <l_0306@naver.com>
2021-09-29 12:30:08 +0900

    fix: pr 템플릿 생성으로 리셋
    실수

---
+++ static/js/modal.js
@@ -0,0 +1,12 @@
+$('#login_password_eye').on("mousedown", function(){
+    $('#login_password').attr('type',"text");
+}).on('mouseup mouseleave', function() {
+    $('#login_password').attr('type',"password");
+});
+
+$('#signup_password_eye').on("mousedown", function(){
+    $('#signup_password').attr('type',"text");
+}).on('mouseup mouseleave', function() {
+    $('#signup_password').attr('type',"password");
+});
+
